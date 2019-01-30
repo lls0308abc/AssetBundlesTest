f5b844d4a26910ed8632f7e00dc05b5715db7e30
Jin An <lls0308abc@gmail.com>
2019-01-30 17:19:12 +0800

    Auto input skin ID when runnung

--- Assets/Scripts/SkinList.cs
+++ Assets/Scripts/SkinList.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkinList : MonoBehaviour {
 
     public string[] skinIDs;
+
+    public InputField InputField;
+    private void Start()
+    {
+        InputField.text = skinIDs[0];
+    }
 }
